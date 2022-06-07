f71e9094b9d1582adab15b7e42c3d72635f32c36
Andrei Calin <andrei.calin@toptal.com>
2022-06-07 15:01:57 +0300

    add files

--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -1,9 +1,9 @@
-val = 100
+val = "100"
 
 RSpec.describe 'redis service' do
   specify do
-    redis = Redis.new(host: 'localhost', port: 6379)
+    redis = Redis.new(host: 'cache', port: 6379)
     redis.set("key", val)
-    expect(redis.get("key")).to be(val)
+    expect(redis.get("key")).to eq(val)
   end
 end
